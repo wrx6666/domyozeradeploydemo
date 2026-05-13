3e7799afce87a2526663adcbb414dec854a04a4e
wrx6666 <166891935+wrx6666@users.noreply.github.com>
2026-05-13 21:15:24 +0300

    2сomitsettings
    2сomitsettings

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://wrx6666.github.io',
+  base: '/domyozeradeploydemo',
   compressHTML: false,
   vite: {
     build: {
