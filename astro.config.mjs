842fcc9e80609af728e6aa35490fc0955eb8c968
wrx6666 <166891935+wrx6666@users.noreply.github.com>
2026-05-13 20:30:31 +0300

    firstprojetcomit
    firstprojetcomit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  compressHTML: false,
+  vite: {
+    build: {
+      minify: false,
+    },
+  },
+});
